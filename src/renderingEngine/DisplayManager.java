4b1049e5c3eddef581e3e68c224d65b98cd9e94c
Mart Ammon <mart@liovic.eu>
2017-07-21 09:45:08 +0300

    Added a Displaymanager class to create our display

---
+++ src/renderingEngine/DisplayManager.java
@@ -0,0 +1,60 @@
+package renderingEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.opengl.*;
+
+public class DisplayManager
+{
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    public static final int FPS_CAP = 400;
+
+    private static long lastFrameTime;
+    private static float delta;
+
+    public static void createDisplay()
+    {
+        ContextAttribs attribs = new ContextAttribs(3,2).withForwardCompatible(true).withProfileCore(true);
+        PixelFormat pixelFormat = new PixelFormat();
+
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.setResizable(true);
+            Display.create(pixelFormat, attribs);
+            Display.setTitle("Multiplayer World 2");
+        }
+        catch(LWJGLException e)
+        {
+            e.printStackTrace();
+        }
+
+        GL11.glViewport(0,0, Display.getWidth(), Display.getHeight());
+        lastFrameTime = getCurrentTime();
+    }
+
+    public static void updateDisplay()
+    {
+        GL11.glViewport(0,0, Display.getWidth(), Display.getHeight());
+        Display.sync(FPS_CAP);
+        Display.update();
+        long currentFrameTime = getCurrentTime();
+        delta = currentFrameTime - lastFrameTime;
+        lastFrameTime = currentFrameTime;
+    }
+
+    public static float getFrameTimeMillisec()
+    {
+        return delta;
+    }
+
+    public static void closeDisplay()
+    {
+        Display.destroy();
+    }
+
+    private static long getCurrentTime()
+    {
+        return Sys.getTime()*1000/Sys.getTimerResolution();
+    }
+}
