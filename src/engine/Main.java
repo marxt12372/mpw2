15ccc3e98592140c66c5a6b4f52d6cd094b56a04
Mart Ammon <mart@liovic.eu>
2017-07-21 09:51:02 +0300

    Made the display show up

--- src/engine/Main.java
+++ src/engine/Main.java
@@ -1,5 +1,7 @@
 package engine;
 
+import org.lwjgl.opengl.Display;
+import renderingEngine.DisplayManager;
 import toolbox.*;
 
 
@@ -7,5 +9,11 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		DisplayManager.createDisplay();
+		while(!Display.isCloseRequested())
+		{
+			DisplayManager.updateDisplay();
+		}
+		DisplayManager.closeDisplay();
 	}
 }
